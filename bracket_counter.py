25d79fdc0fe7b4ec67563957bf92ab5ba9ffbcf8
Darshil Papalkar <67659174+Darshil-Papalkar@users.noreply.github.com>
2020-09-04 10:59:46 +0530

    Update-1

--- bracket_counter.py
+++ bracket_counter.py
@@ -2,28 +2,31 @@ def bracket_counter(bracket):
     count = 0
     count1 = 0
     count2 = 0
-    for char in bracket:
-        if char == "(":
-            count += 1
-        elif char == ")":
-            if count != 0:
-                count -= 1
-            else:
-                return f"{bracket} --> False"
-        if char == "{":
-            count1 += 1
-        elif char == "}":
-            if count1 != 0:
-                count1 -= 1
-            else:
-                return f"{bracket} --> False"
-        if char == "[":
-            count2 += 1
-        elif char == "]":
-            if count2 != 0:
-                count2 -= 1
-            else:
-                return f"{bracket} --> False"
+    for i in range(len(bracket)):
+        try:
+            if bracket[i] == "(" and (bracket[i + 1] != "}" and bracket[i + 1] != "]"):
+                count += 1
+            elif bracket[i] == ")":
+                if count != 0:
+                    count -= 1
+                else:
+                    return f"{bracket} --> False"
+            elif bracket[i] == "{" and (bracket[i + 1] != ")" and bracket[i + 1] != "]"):
+                count1 += 1
+            elif bracket[i] == "}":
+                if count1 != 0:
+                    count1 -= 1
+                else:
+                    return f"{bracket} --> False"
+            elif bracket[i] == "[" and (bracket[i + 1] != ")" and bracket[i + 1] != "}"):
+                count2 += 1
+            elif bracket[i] == "]":
+                if count2 != 0:
+                    count2 -= 1
+                else:
+                    return f"{bracket} --> False"
+        except Exception as e:
+            return f"{bracket} --> False"
     if count == 0 and count1 == 0 and count2 == 0:
         return f"{bracket} --> True"
     else:
@@ -31,5 +34,8 @@ def bracket_counter(bracket):
 
 
 if __name__ == "__main__":
-    for _ in range(int(input("Enter the number of times to run loop "))):
-        print(bracket_counter(input()))
+    try:
+        for _ in range(int(input("Enter the number of times to run loop "))):
+            print(bracket_counter(input()))
+    except Exception as e:
+        print(e)
